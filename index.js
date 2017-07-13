8b739251468348e9bee56a0f9ce05523e0322ac6
bredele <olivier.wietrich@gmail.com>
2017-07-13 14:39:38 -0600

    Decode data only if type exists

--- index.js
+++ index.js
@@ -27,21 +27,21 @@ module.exports = function (req, cb) {
   if (!~allowed.indexOf(req.method)) return cb()
   try {
     const type = parse(req).type
+    if (type === 'application/x-www-form-urlencoded') {
+      let list = []
+      req.on('data', (chunk) => list.push(chunk))
+      req.on('end', () => {
+        const buffer = Buffer.concat(list)
+        cb(query(buffer.toString()))
+      })
+    } else if (type === 'multipart/form-data') {
+      (new Form()).parse(req, function(err, fields, files) {
+        const obj = {}
+        if (fields) Object.keys(fields).map(key => obj[key] = fields[key][0])
+        cb(obj)
+      })
+    }
   } catch (e) {
     cb({})
-  }
-  if (type === 'application/x-www-form-urlencoded') {
-    let list = []
-    req.on('data', (chunk) => list.push(chunk))
-    req.on('end', () => {
-      const buffer = Buffer.concat(list)
-      cb(query(buffer.toString()))
-    })
-  } else if (type === 'multipart/form-data') {
-    (new Form()).parse(req, function(err, fields, files) {
-      const obj = {}
-      if (fields) Object.keys(fields).map(key => obj[key] = fields[key][0])
-      cb(obj)
-    })
   }
 }
